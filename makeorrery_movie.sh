3fbf54908b49f89ac72abf35985bde978c67cf1b
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2019-06-19 22:53:50 -0400

    getting movies to work on macs

--- makeorrery_movie.sh
+++ makeorrery_movie.sh
@@ -2,6 +2,6 @@
 # first argument is directory with input/output files
 # second argument is the name of the output gif (without the directory)
 # third argument is the frame rate (fps)
-
+# pix_fmt is used to be compatible with apple devices.
 # make the mp4 using a particular frame rate.
-ffmpeg -framerate $3 -i $1fig%04d.png -c:v libx264 -crf 18 $1$2
+ffmpeg -framerate $3 -i $1fig%04d.png -c:v libx264 -pix_fmt yuv420p -crf 18 $1$2
